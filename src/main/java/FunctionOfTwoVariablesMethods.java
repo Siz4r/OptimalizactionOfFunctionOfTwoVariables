82bb123a6af367de9beefa8c2162e473294bbc7c
Siz4r <zaq134215@wp.pl>
2022-04-21 08:48:15 +0300

    Poprawic pochodne

--- src/main/java/FunctionOfTwoVariablesMethods.java
+++ src/main/java/FunctionOfTwoVariablesMethods.java
@@ -18,4 +18,8 @@ abstract class FunctionOfTwoVariablesMethods {
         return (Math.abs(v2.getX() - v1.getX()) <= command.getEpsilon() &&
                 Math.abs(v2.getY() - v1.getY()) <= command.getEpsilon()) ||(Math.abs(x) <= command.getEpsilon() && Math.abs(y) <= command.getEpsilon());
     }
+
+    boolean stopCondition(Vector v) {
+        return Math.abs(v.getX()) < command.getEpsilon() && Math.abs(v.getY()) < command.getEpsilon();
+    }
 }
