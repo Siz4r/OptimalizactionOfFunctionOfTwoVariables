fd6863bc2c2b559335d88d5a2ea69d2ff2d85d8f
Siz4r <zaq134215@wp.pl>
2022-04-27 16:34:20 +0200

    Jakies zmiany

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         var command  = new FunctionOfTwoVariablesCommand(
-                0.00001, new Vector(3, 2)
+                0.00001, new Vector(3, 3)
         );
 
         var methods = List.of(new NewtonsMethod(command), new SteepestDescentMethod(command));
